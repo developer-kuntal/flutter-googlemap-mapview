9354d7268e61a62ae0e46eaa2fac91e3ad70388d
developer-kuntal <kuntal.iiest@gmail.com>
2019-04-15 22:08:49 +0530

    Flutter Google map mapview update at 10.08 PM 4/15/2019

--- lib/main.dart
+++ lib/main.dart
@@ -39,13 +39,37 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  MapView mapView = new MapView();
+  List<Marker> markers = <Marker> [
+
+  ];
+  
+  displayMap() {
+   mapView.show(new MapOptions(
+     mapViewType: MapViewType.normal,
+     initialCameraPosition: new CameraPosition(new Location(35.22, -101.83), 15.0),
+     showUserLocation: false,
+     title: 'Google Map'
+   )); 
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: , 
+      body: new Center(
+        child: Container(
+          child: RaisedButton(
+            child: Text("Tap me"),
+            color: Colors.blue,
+            textColor: Colors.white,
+            elevation: 7.0,
+            onPressed: displayMap(),
+          ),
+        ),
+      ), 
     );
   }
 }
